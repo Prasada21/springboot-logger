7a9c9dbcf1f8bcbc87c51e7f85bcb0d143c3dc78
user <user@DESKTOP-HRR6100>
2022-09-04 17:07:48 +0530

    springboot-logger

---
+++ spring-boot-loggerwith-sl4j-logback/src/test/java/com/prasad/springbootloggerwithsl4jlogback/SpringBootLoggerwithSl4jLogbackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.prasad.springbootloggerwithsl4jlogback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootLoggerwithSl4jLogbackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
